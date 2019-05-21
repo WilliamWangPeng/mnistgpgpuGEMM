11ac49955b608f5fd4f3044cc8127254f62a8ec2
shengyushen <shengyushen@aliyun.com>
2019-05-21 06:21:31 +0000

    adding new script

---
+++ runall.sh
@@ -0,0 +1,20 @@
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l100 gpgpusim.config ;./mnistCUDNN  1  1 |tee xx10_bs1_l100
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l200 gpgpusim.config ;./mnistCUDNN  1  1 |tee xx10_bs1_l200
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l400 gpgpusim.config ;./mnistCUDNN  1  1 |tee xx10_bs1_l400
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l800 gpgpusim.config ;./mnistCUDNN  1  1 |tee xx10_bs1_l800
+
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l100 gpgpusim.config ;./mnistCUDNN  16  1 |tee xx10_bs16_l100
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l200 gpgpusim.config ;./mnistCUDNN  16  1 |tee xx10_bs16_l200
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l400 gpgpusim.config ;./mnistCUDNN  16  1 |tee xx10_bs16_l400
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l800 gpgpusim.config ;./mnistCUDNN  16  1 |tee xx10_bs16_l800
+
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l100 gpgpusim.config ;./mnistCUDNN  64  1 |tee xx10_bs64_l100
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l200 gpgpusim.config ;./mnistCUDNN  64  1 |tee xx10_bs64_l200
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l400 gpgpusim.config ;./mnistCUDNN  64  1 |tee xx10_bs64_l400
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l800 gpgpusim.config ;./mnistCUDNN  64  1 |tee xx10_bs64_l800
+
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l100 gpgpusim.config ;./mnistCUDNN  256  1 |tee xx10_bs256_l100
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l200 gpgpusim.config ;./mnistCUDNN  256  1 |tee xx10_bs256_l200
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l400 gpgpusim.config ;./mnistCUDNN  256  1 |tee xx10_bs256_l400
+rm -f *.ptx *.ptxas _app_cuda_version_* ;cp gpgpusim.config_l800 gpgpusim.config ;./mnistCUDNN  256  1 |tee xx10_bs256_l800
+
